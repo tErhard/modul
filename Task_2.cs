1a6b5b5b80d4c082fd0ba52f8abbb8276bd581fa
telingererhard <93592012+tErhard@users.noreply.github.com>
2022-04-19 13:02:09 +0300

    Add files via upload

---
+++ Task_2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Task_1
+{
+	public Task_1()
+	{
+		let n = prompt("Введіть кількість елементів");
+		let arr = new Array();
+		for (let i = 0; i < n; i++)
+		{
+			arr.push(Math.round(Math.random() * 100));
+
+		}
+		alert(`1 масив: ${ arr}`);
+		var arr2 = arr.filter((x, i, arr) => arr[i] > arr[0]);
+		alert(`2 масив: ${ arr2}`);
+	}
+}
